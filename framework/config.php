a2b0350a54b9e94be9e0f67cb987a35f47eac7a8
Marinsan <cristianmarin@iesebre.com>
2018-09-21 18:59:11 +0200

    Separació dades conexió a fitxer config (en proces)

---
+++ framework/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database' => [
+        'name' => 'php_learning',
+        'username' => 'debian-sys-maint',
+        'password' => 'h3vBPc4hwZa3734t',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options'
+    ]
+];
