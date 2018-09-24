2362c6b9be9e3eed4642ae24f405c5a8ea570da0
Marin <cristianmarin@iesebre.com>
2018-09-24 19:55:05 +0200

    Add files via upload

---
+++ framework/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'type' => 'mysql',
+        'name' => 'php_learning',
+        'username' => 'debian-sys-maint',
+        'password' => 'h3vBPc4hwZa3734t',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => 'PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION'
+    ]
+];
